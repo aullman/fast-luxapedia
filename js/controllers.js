b10d656cc4aa05d49399aab278ee832164ea0637
Adam Ullman <adam@tokbox.com>
2014-09-16 19:38:43 +1000

    Added evolution information

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,11 @@
+var pokemonDictionary = {};
 angular.module('luxapedia')
     .controller('PokedexCtrl', ['$scope', 'Pokedex', function($scope, Pokedex) {
-        $scope.pokedex = Pokedex.query();
+        $scope.pokedex = Pokedex.query(function () {
+          $scope.pokedex.forEach(function (pokemon) {
+            pokemonDictionary[pokemon.id] = pokemon.name;
+          });
+        });
         
         $scope.getPokemon = function (uri) {
             alert(uri);
@@ -9,5 +14,11 @@ angular.module('luxapedia')
     .controller('PokemonDetailCtrl', ['$scope', '$routeParams', 'Pokemon',
          function ($scope, $routeParams, Pokemon) {
              $scope.pokemonId = $routeParams.pokemonId;
-             $scope.pokemon = Pokemon.get({id: $routeParams.pokemonId});
+             $scope.pokemon = Pokemon.get({id: $routeParams.pokemonId}, function (pokemon) {
+               pokemon.evolutions.forEach(function (evolution) {
+                 evolution.id = evolution.resource_uri.match(/api\/v1\/pokemon\/(\d+)\//)[1];
+                 evolution.name = pokemonDictionary[evolution.id];
+               });
+               
+             });
     }]);
